f500236ae538aef15671cf9169f84c83b732baf2
Nathan Gerryts <nathgerr@gmail.com>
2022-12-04 00:32:22 -0500

    Updated Day 4

--- Day4/Day4P2.py
+++ Day4/Day4P2.py
@@ -7,7 +7,7 @@ for x in file:
     pair = x.split(',')
     first = range(int(pair[0].split("-")[0]), int(pair[0].split("-")[1])+1)
     second = range(int(pair[1].split("-")[0]), int(pair[1].split("-")[1])+1)
-    if not set(first).isdisjoint(second) or not set(second).isdisjoint(first):
+    if not set(first).isdisjoint(second):
         total += 1
         
 print(total)

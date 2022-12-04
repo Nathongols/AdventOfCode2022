5c9890e85d8ca148aaf89ebec3abc37a2c474323
Nathan Gerryts <nathgerr@gmail.com>
2022-12-04 00:30:26 -0500

    Day 4

---
+++ Day4/Day4P2.py
@@ -0,0 +1,13 @@
+
+file = open(r"C:\Users\Nathg\Documents\School\AdventOfCoding\Day4\input.txt", "r")
+
+total = 0
+
+for x in file:
+    pair = x.split(',')
+    first = range(int(pair[0].split("-")[0]), int(pair[0].split("-")[1])+1)
+    second = range(int(pair[1].split("-")[0]), int(pair[1].split("-")[1])+1)
+    if not set(first).isdisjoint(second) or not set(second).isdisjoint(first):
+        total += 1
+        
+print(total)
